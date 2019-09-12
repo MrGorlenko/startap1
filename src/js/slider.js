7ceaf85594b59067483197a63fb4a38497515660
Artem <artem@MacBook-Air-Artem.local>
2019-09-12 10:59:30 +0300

    Тестовая версия сайта номер 1

---
+++ src/js/slider.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.slider__base').slick({
+        infinite: true,
+        arrows: false,
+        centerPadding: '0',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        pauseOnHover: false,
+        swipe: false,
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+                fade: false
+                }
+            }
+        ]
+    });
+  });
